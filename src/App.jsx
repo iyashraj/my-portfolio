77329f195c305f60c8eb27fe05236573dd4795a6
iyashraj <yashr3037@gmail.com>
2024-02-18 14:41:18 +0530

    trying to update build

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Experience from './components/Experience'
 const App = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Greet />
       <About />
       <Experience />
